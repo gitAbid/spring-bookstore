af92250ec81d5a40ef33fc712284f1a35d7707f3
Abid Hasan <abidh@hsenidmobile.com>
2020-01-13 10:15:48 +0600

    code review project kickoff

---
+++ src/main/kotlin/com/abid/springbookstore/SpringBookstoreApplication.kt
@@ -0,0 +1,11 @@
+package com.abid.springbookstore
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBookstoreApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringBookstoreApplication>(*args)
+}
---
+++ src/test/kotlin/com/abid/springbookstore/SpringBookstoreApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.abid.springbookstore
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBookstoreApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
